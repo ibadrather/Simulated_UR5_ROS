35acb50c3bd25a13d06a9183ca3dd8b1613b3ce0
Ibad Rather <ibad.rather.ir@gmail.com>
2024-12-11 00:05:40 +0100

    target pose described via cli

--- src/ur5_sim_motion_control/src/cartesian_position_node.cpp
+++ src/ur5_sim_motion_control/src/cartesian_position_node.cpp
@@ -7,12 +7,14 @@
 #include <kdl/chainfksolverpos_recursive.hpp>
 #include <kdl/chainiksolverpos_lma.hpp>
 #include <kdl/jntarray.hpp>
+#include <std_msgs/Empty.h>
 
 const std::vector<std::string> JOINT_NAMES = {
     "shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint",
     "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"
 };
 
+// Function to compute inverse kinematics (IK)
 bool computeIK(const KDL::Chain& chain, const KDL::JntArray& initial_joints,
                const geometry_msgs::Pose& target_pose, KDL::JntArray& result_joints) {
     KDL::ChainIkSolverPos_LMA ik_solver(chain);
@@ -41,6 +43,7 @@ bool computeIK(const KDL::Chain& chain, const KDL::JntArray& initial_joints,
     return true;
 }
 
+// Function to publish joint trajectory to move robot
 void publishTrajectory(const ros::Publisher& joint_pub,
                        const KDL::JntArray& joint_positions) {
     trajectory_msgs::JointTrajectory trajectory_msg;
@@ -60,6 +63,47 @@ void publishTrajectory(const ros::Publisher& joint_pub,
     trajectory_msg.points.push_back(point);
     joint_pub.publish(trajectory_msg);
     ROS_INFO("Published trajectory to target position.");
+}
+
+// Function to continuously publish current position and joint states
+void publishCurrentPosition(const ros::Publisher& joint_pub, const KDL::Chain& chain,
+                            const KDL::JntArray& joint_positions) {
+    KDL::ChainFkSolverPos_recursive fk_solver(chain);
+    KDL::Frame current_pose;
+    fk_solver.JntToCart(joint_positions, current_pose);
+
+    // Convert current pose to geometry_msgs::Pose
+    geometry_msgs::Pose current_pose_msg;
+    current_pose_msg.position.x = current_pose.p.x();
+    current_pose_msg.position.y = current_pose.p.y();
+    current_pose_msg.position.z = current_pose.p.z();
+    
+    // Get quaternion from rotation matrix
+    double x, y, z, w;
+    current_pose.M.GetQuaternion(x, y, z, w);
+    current_pose_msg.orientation.x = x;
+    current_pose_msg.orientation.y = y;
+    current_pose_msg.orientation.z = z;
+    current_pose_msg.orientation.w = w;
+
+    ROS_INFO("Current position: [%.2f, %.2f, %.2f]", current_pose_msg.position.x, current_pose_msg.position.y, current_pose_msg.position.z);
+
+    // Publish joint states
+    publishTrajectory(joint_pub, joint_positions);
+}
+
+// Callback function to move to a target pose
+void moveToPoseCallback(const geometry_msgs::Pose::ConstPtr& target_pose_msg,
+                        const ros::Publisher& joint_pub, const KDL::Chain& chain, KDL::JntArray& initial_joints) {
+    KDL::JntArray result_joints(chain.getNrOfJoints());
+
+    ROS_INFO("Requested to go to a pose");
+
+    if (computeIK(chain, initial_joints, *target_pose_msg, result_joints)) {
+        publishTrajectory(joint_pub, result_joints);
+    } else {
+        ROS_ERROR("Failed to compute IK for target pose.");
+    }
 }
 
 int main(int argc, char** argv) {
@@ -85,39 +129,26 @@ int main(int argc, char** argv) {
     ros::Publisher joint_pub = nh.advertise<trajectory_msgs::JointTrajectory>(
         "/eff_joint_traj_controller/command", 10);
 
-    // Define start and end poses
-    geometry_msgs::Pose start_pose, end_pose;
+    ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::Pose>(
+        "/target_pose", 10, boost::bind(moveToPoseCallback, _1, joint_pub, kdl_chain, KDL::JntArray(kdl_chain.getNrOfJoints())));
+
+    // Define start pose
+    geometry_msgs::Pose start_pose;
     start_pose.position.x = 0.5;
     start_pose.position.y = 0.5;
     start_pose.position.z = 0.5;
     start_pose.orientation.w = 1.0;
 
-    end_pose.position.x = -0.5;
-    end_pose.position.y = -0.5;
-    end_pose.position.z = 0.5;
-    end_pose.orientation.w = 1.0;
-
-    ROS_INFO("Start pose: [%.2f, %.2f, %.2f]", start_pose.position.x, start_pose.position.y, start_pose.position.z);
-    ROS_INFO("End pose: [%.2f, %.2f, %.2f]", end_pose.position.x, end_pose.position.y, end_pose.position.z);
-
+    // Initial joint positions
     KDL::JntArray initial_joints(kdl_chain.getNrOfJoints());
     initial_joints.data.setZero();
 
-    geometry_msgs::Pose target_pose = start_pose;
-
-    // Repeat back-and-forth motion
-    int cycles = 10; // Number of cycles to repeat
-    for (int i = 0; i < cycles && ros::ok(); ++i) {
-        KDL::JntArray result_joints(kdl_chain.getNrOfJoints());
-        if (computeIK(kdl_chain, initial_joints, target_pose, result_joints)) {
-            publishTrajectory(joint_pub, result_joints);
-            ROS_INFO("Moving to %s pose.", (target_pose.position.x == start_pose.position.x) ? "start" : "end");
-        } else {
-            ROS_ERROR("Failed to compute IK for target pose. Skipping...");
-        }
-
-        target_pose = (target_pose.position.x == start_pose.position.x) ? end_pose : start_pose;
-        ros::Duration(10.0).sleep(); // Pause for motion execution
+    // Publish current position continuously
+    ros::Rate loop_rate(10);  // 10 Hz
+    while (ros::ok()) {
+        // publishCurrentPosition(joint_pub, kdl_chain, initial_joints);
+        ros::spinOnce();
+        loop_rate.sleep();
     }
 
     return 0;
